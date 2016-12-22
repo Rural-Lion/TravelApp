002668c51f842c14bb2afa8002928f4f8bd91da2
Romain DEJEAN <romain.dejean@gadz.org>
2016-12-21 18:21:28 -0800

    Setup-BE:Sequelize connection-RD

---
+++ database/database.js
@@ -0,0 +1,16 @@
+let Sequelize = require('sequelize');
+
+const dnName = 'rurallion';
+const username = 'rurallion';
+const password = 'hackreactor69';
+
+var sequelize = new Sequelize(dnName, username, password, {
+  host: 'rurallion.cgnjbb8knqep.us-west-2.rds.amazonaws.com',
+  port: '3306',
+  dialectOptions: {
+    ssl: 'Amazon RDS'
+  }
+});
+
+
+module.exports = sequelize;
