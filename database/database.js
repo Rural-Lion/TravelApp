89e6ad6038538d5a1900158dea55126441384967
Romain DEJEAN <romain.dejean@gadz.org>
2016-12-22 20:35:50 -0800

    Setup-BE: Connected app to local DB - RD

--- database/database.js
+++ database/database.js
@@ -1,15 +1,25 @@
 let Sequelize = require('sequelize');
 require('dotenv-safe').load();
 
-var db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialectOptions: {
-    ssl: 'Amazon RDS'
-  }
+//TO BE UNCOMMENTED WHEN USING AMAZON RDS
+// var db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   dialectOptions: {
+//     ssl: 'Amazon RDS'
+//   }
+// });
+
+//TEMPORARY USE WHILE RUNNING DB ON LOCAL MACHINE
+let username = 'root';
+let password = '';
+
+var db = new Sequelize('rurallion', username, password, {
+  host: 'localhost',
+  dialect: 'mysql',
 });
 
-//Verifying DB Connection
+//Verifying DB Connection~
 db.authenticate().then(function(err) {
   console.log('Connection has been established successfully'); 
 })
