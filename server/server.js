f3db6302a886d1a00f1b26677dc4ea389e0807f9
elliottabirch <elliottabirch@gmail.com>
2016-12-23 10:33:49 -0800

    FIX-BE:delete duplicate express declaration-EB

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 
 const express = require('express');
 const db = require('../database/database.js');
-const express = require('express');
 const bodyparser = require('body-parser');
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
@@ -16,7 +15,7 @@ const app = express();
 app.use(bodyparser.json());
 app.use(webpackMiddleware(webpackConfig, {}));
 app.use(webpackHotMiddleware(webpackConfig));
-app.use(express.static(`${__dirname  }./../client/public`));
+app.use(express.static(`${__dirname}./../client/public`));
 
 app.listen(port);
 console.log('listening on', port);
