b9fae44f60f5761917317d004e14b8a1bb1a8980
Romain DEJEAN <romain.dejean@gadz.org>
2016-12-22 22:09:18 -0800

    Setup-BE: Set up strings lengths in schemas - RD

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const bodyparser = require('body-parser');
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const webpackConfig = require('webpack')(require('../webpack.config.js'));
+const schemas = require('../database/schemas.js');
 
 require('dotenv-safe').load();
 
