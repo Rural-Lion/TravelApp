520164822aea939b5a45c6dc88353c579a19d8aa
elliottabirch <elliottabirch@gmail.com>
2016-12-22 23:56:49 -0800

    REACT-FE:pseudocode fe pages-EB

---
+++ client/app/app.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+
+const ACTIVITIES = [];
+const INTERESTS = [];
+
+
+function FancyBorder(props) {
+
+}
+class App extends Component {
+
+// class landingPage
+  // render
+    // header
+    // inputs
+    // choose interests text
+    // interest buttons
+
+
+// class planVacation Button
+  // pass inputs
+  // pass selected buttons
+
+// class inputs
+  // pass input values up
+  // render inputs
+
+// class interest buttons
+  // pass up selected buttons
+  // render
+    // interest buttons with given array param
+
+// class interest button
+  // passes up individual data when selected
+  // renders
+    // button with given param
+
+
+// class resultsPage
+  // takes given array of facilities
+  // renders
+    // nav bar
+    // map header
+    // map
+    // activity header
+    // list of activities
+
+// class nav bar
+  // renders
+    // finalize button
+    // go back button
+
+// class map
+  // renders
+    // google map given
+    // activityMapButtons
+
+// class activityMapButtons
+  // renders
+    // button with given param
+
+// class activityList
+  // renders
+    // list of activityListEntrys
+
+// class ActivityListEntry
+  // renders
+    // entry with given activity object
+
+}
+
+export default App;
