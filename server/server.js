0c61af878834deeb805a1e27fc641d13a7a37554
elliottabirch <elliottabirch@gmail.com>
2016-12-22 20:46:39 -0800

    SETUP-FE:setup webpack hot reload-EB

--- server/server.js
+++ server/server.js
@@ -1,17 +1,23 @@
-let express = require('express');
-let db = require('../database/database.js');
-let bodyparser = require('body-parser');
+
+const express = require('express');
+const db = require('../database/database.js');
+const express = require('express');
+const bodyparser = require('body-parser');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const webpackConfig = require('webpack')(require('../webpack.config.js'));
+
 require('dotenv-safe').load();
 
-let port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
-let app = express();
+const app = express();
 
 app.use(bodyparser.json());
-
-app.use(express.static(__dirname + './../client/public'));
+app.use(webpackMiddleware(webpackConfig, {}));
+app.use(webpackHotMiddleware(webpackConfig));
+app.use(express.static(`${__dirname  }./../client/public`));
 
 app.listen(port);
 console.log('listening on', port);
-
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,11 @@ const BUILD_DIR = path.resolve(__dirname, 'client/public');
 const APP_DIR = path.resolve(__dirname, 'client/app');
 
 const config = {
-  entry: APP_DIR + '/app.js',
+  entry: [
+    'webpack-hot-middleware/client',
+    'react-hot-loader/patch',
+    APP_DIR + '/index.jsx'
+  ],
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
@@ -22,12 +26,16 @@ const config = {
       }
     ]
   },
-
   plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     new DotenvPlugin({
       sample: './.env.example',
       path: './.env'
-    })
+    }),
+    new webpack.SourceMapDevToolPlugin(
+            'bundle.js.map', null,
+            "[absolute-resource-path]", "[absolute-resource-path]")
   ]
 
 
