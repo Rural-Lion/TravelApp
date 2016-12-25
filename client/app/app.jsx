0313c400a42418377647977a9ac72e87f3cc1bc2
elliottabirch <elliottabirch@gmail.com>
2016-12-25 15:43:28 -0600

    REACT-FE:clean up code and comment-EB

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -1,10 +1,27 @@
 import React, { Component, PropTypes } from 'react';
 import axios from 'axios';
-// import activitiesJSON from './activities';
 
+// FRONT END COMPONENT TREE
+
+// App - all state is held here for now
+  // LandingPage
+    // Inputs
+    // PlanVacationButton
+    // InterestButtons
+      // interestButton
+  // ResultsPage
+    // NavBar
+    // ActivityList
+      // ActivityListEntry
+    // Map
+
+// for questions on the proptypes after the components, check it out on docs. they are pretty cool
+
+
+// interests for interest buttons are hard coded in
 const INTERESTS = ['hiking', 'skiing', 'boating', 'historical', 'mountains', 'fishing'];
 
-// fancy border to help with styling and page positioning
+// function for creating objects from the results of api request to the US website
 const generateActivities = res => res.map(({
                   RECAREAADDRESS: [{ PostalCode, RecAreaStreetAddress1, City, AddressStateCode }],
                   RecAreaLatitude,
@@ -28,6 +45,9 @@ const generateActivities = res => res.map(({
   };
 });
 
+// all components are being passed through this function,
+// in order to put borders around kthem to make styling easier
+  // uncomment the classes on the css page to enable the borders
 const FancyBorder = function (props) {
   return (
     <div className={`FancyBorder FancyBorder-${props.color}`}>
@@ -41,14 +61,13 @@ FancyBorder.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element),
 };
 
-// class interest button
-  // passes up individual data when selected
-  // renders
-    // button with given param
 
 const InterestButton = props => (
   <FancyBorder color="blue" class="interestButton">
-    <button className="centered" onClick={(e) => { props.handleInterestButtonClick(e); }}>{props.interest}</button>
+    <button
+      className="centered"
+      onClick={(e) => { props.handleInterestButtonClick(e); }}
+    >{props.interest}</button>
   </FancyBorder>
   );
 
@@ -57,19 +76,19 @@ InterestButton.propTypes = {
   handleInterestButtonClick: PropTypes.func,
 };
 
-
-// class interest buttons
-  // pass up selected buttons
-  // render
-    // interest buttons with given array param
-
 const InterestButtons = (props) => {
   const interests = props.interests;
+  // generates buttons based off of INTERESTS array. currently hard coded
   const buttons = interests.map((interest, index) =>
     <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-      <InterestButton interest={interest} key={index} handleInterestButtonClick={props.handleInterestButtonClick} /></div>);
+      <InterestButton
+        interest={interest}
+        key={index}
+        handleInterestButtonClick={props.handleInterestButtonClick}
+      /></div>);
 
   return (
+    // renders buttons
     <FancyBorder color="green">
       {buttons}
     </FancyBorder>
@@ -82,41 +101,74 @@ InterestButtons.propTypes = {
 };
 
 
-// class planVacation Button
-  // pass inputs
-  // pass selected buttons
-
 const PlanVacationButton = props => (
-
   <FancyBorder color="yellow">
-    <button className="centered" onClick={() => props.handlePlanButtonClick()}>Plan My Vacation</button>
+    <button
+      className="centered"
+      onClick={() => props.handlePlanButtonClick()}
+    >
+    Plan My Vacation
+    </button>
   </FancyBorder>
 
 );
 
-// class inputs
-  // pass input values up
-  // render inputs
+PlanVacationButton.propTypes = {
+  handlePlanButtonClick: PropTypes.func,
+};
+
 const Inputs = props => (
   <FancyBorder color="yellow" >
-    <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3"><FancyBorder color="green"><input data-tag="budgetOfTrip" onChange={e => props.handleChange(e)} className="lpInput" type="text" /></FancyBorder></div>
-    <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3"><FancyBorder color="green"><input data-tag="lengthOfTrip" onChange={e => props.handleChange(e)} className="lpInput" type="text" /></FancyBorder></div>
-    <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3"><FancyBorder color="green"><input data-tag="startingLocation" onChange={e => props.handleChange(e)} className="lpInput" type="text" /></FancyBorder></div>
-    <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3"><FancyBorder color="green"><input data-tag="distanceOfTrip" onChange={e => props.handleChange(e)} className="lpInput" type="text" /></FancyBorder></div>
+    <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+      <FancyBorder color="green">
+      [//input for budget]
+        <input
+          data-tag="budgetOfTrip"
+          onChange={e => props.handleChange(e)}
+          className="lpInput"
+          type="text"
+        />
+      </FancyBorder>
+    </div>
+    <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+      <FancyBorder color="green">
+      [//input for length of trip]
+        <input
+          data-tag="lengthOfTrip"
+          onChange={e => props.handleChange(e)}
+          className="lpInput"
+          type="text"
+        />
+      </FancyBorder>
+    </div>
+    <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+      <FancyBorder color="green">
+      [// input for starting location]
+        <input
+          data-tag="startingLocation"
+          onChange={e => props.handleChange(e)}
+          className="lpInput"
+          type="text"
+        />
+      </FancyBorder>
+    </div>
+    <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+      <FancyBorder color="green">
+      [// input for distance willing to travel]
+        <input
+          data-tag="distanceOfTrip"
+          onChange={e => props.handleChange(e)}
+          className="lpInput"
+          type="text"
+        />
+      </FancyBorder>
+    </div>
   </FancyBorder>
   );
 
 Inputs.propTypes = {
   handleChange: PropTypes.func,
 };
-
-
-// class landingPage
-  // render
-    // header
-    // inputs
-    // choose interests text
-    // interest buttons
 
 const LandingPage = function (props) {
   return (
@@ -130,22 +182,32 @@ const LandingPage = function (props) {
         <div className="row">
           <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8 col-centered">
             <div className="row">
+              [// renders input boxes]
               <Inputs handleChange={props.handleChange} />
             </div>
           </div>
         </div>
         <div className="row">
-          <FancyBorder color="yellow"><div className="text-center"><h5 >Choose at least 2 interests</h5></div></FancyBorder>
+          <FancyBorder color="yellow">
+            <div className="text-center">
+              <h5 >Choose at least 2 interests</h5>
+            </div>
+          </FancyBorder>
         </div>
         <div className="row">
           <div className="col-xs-7 col-sm-7 col-md-7 col-lg-7 col-centered">
             <div className="row">
-              <InterestButtons interests={props.interests} handleInterestButtonClick={props.handleInterestButtonClick} />
+              [// renders interest buttons]
+              <InterestButtons
+                interests={props.interests}
+                handleInterestButtonClick={props.handleInterestButtonClick}
+              />
             </div>
           </div>
         </div>
         <div className="row">
           <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-centered">
+            [// renders plan vacation button]
             <PlanVacationButton handlePlanButtonClick={props.handlePlanButtonClick} />
           </div>
         </div>
@@ -158,22 +220,24 @@ LandingPage.propTypes = {
   interests: PropTypes.arrayOf(PropTypes.string),
   handleInterestButtonClick: PropTypes.func,
   handleChange: PropTypes.func,
+  handlePlanButtonClick: PropTypes.func,
 };
-
-// class nav bar
-  // renders
-    // finalize button
-    // go back button
 
 
 const NavBar = props => (
   <FancyBorder color="yellow">
     <div className="row navBar">
       <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1">
-        <FancyBorder color="green"><button type="button" className="goBackButton btn btn-default">Go Back</button></FancyBorder>
+        <FancyBorder color="green">
+        [//currently doesnt do anything]
+          <button type="button" className="goBackButton btn btn-default">Go Back</button>
+        </FancyBorder>
       </div>
       <div className="col-xs-offset-6 col-sm-offset-6 col-md-offset-6 col-lg-offset-6 col-xs-2 col-sm-2 col-md-2 col-lg-2">
-        <FancyBorder color="green"><button type="button" className="finalizeButton btn btn-default">{'Finalize >'}</button></FancyBorder>
+        <FancyBorder color="green">
+        [//currently doesnt do anything]
+          <button type="button" className="finalizeButton btn btn-default">{'Finalize >'}</button>
+        </FancyBorder>
       </div>
     </div>
   </FancyBorder>
@@ -183,11 +247,8 @@ NavBar.propTypes = {
 
 };
 
-
-// class map
-  // renders
-    // google map given
-    // activityMapButtons
+// INCOMPLETE
+  // has no interaction with user inputs or events
 const Map = props => (
   <div>
     <div className="row">
@@ -207,17 +268,13 @@ const Map = props => (
   );
 
 Map.propTypes = {
-
+  userQuery: PropTypes.object,
+  selectedActivities: PropTypes.array,
 };
 
 
-// class ActivityListEntry
-  // renders
-    // entry with given activity object
-
 const ActivityListEntry = props => (
   <FancyBorder color="green">
-
     <div className="row activityListEntry">
       <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
         <FancyBorder color="blue"><div className="text-center">{props.index}</div></FancyBorder>
@@ -230,7 +287,9 @@ const ActivityListEntry = props => (
           <FancyBorder color="blue"><div>{props.activity.activities.join(', ').toLowerCase()}</div></FancyBorder>
         </div>
         <div className="row">
-          <FancyBorder color="blue"><div dangerouslySetInnerHTML={{ __html: props.activity.description }} /></FancyBorder>
+          <FancyBorder color="blue">
+            <div dangerouslySetInnerHTML={{ __html: props.activity.description }} />
+          </FancyBorder>
         </div>
         <div className="row">
           <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
@@ -240,7 +299,9 @@ const ActivityListEntry = props => (
             <FancyBorder color="blue"><div>{props.activity.address}</div></FancyBorder>
           </div>
           <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <FancyBorder color="blue"><a href={props.activity.website} >{props.activity.website}</a></FancyBorder>
+            <FancyBorder color="blue">
+              <a href={props.activity.website} >{props.activity.website}</a>
+            </FancyBorder>
           </div>
         </div>
       </div>
@@ -249,14 +310,12 @@ const ActivityListEntry = props => (
   );
 
 ActivityListEntry.propTypes = {
-  activities: PropTypes.object,
+  activity: PropTypes.object,
   index: PropTypes.number,
 };
 
-
-// class activityList
-  // renders
-    // list of activityListEntrys
+// renders activities from given array
+  // currently gets array of activities from US API
 const ActivityList = (props) => {
   const activities = props.activities;
   const activityList = activities.map((activity, index) =>
@@ -271,7 +330,6 @@ const ActivityList = (props) => {
         <div className="activityList">
           <FancyBorder color="yellow">
             {activityList}
-
           </FancyBorder>
         </div>
       </div>
@@ -284,25 +342,20 @@ ActivityList.propTypes = {
   activities: PropTypes.arrayOf(PropTypes.object),
 };
 
-// class resultsPage
-  // takes given array of facilities
-  // renders
-    // nav bar
-    // map header
-    // map
-    // activity header
-    // list of activities
 const ResultsPage = props => (
   <div className="resultsPage">
     <FancyBorder color="orange">
+      [//render navbar]
       <div className="row">
         <NavBar />
       </div>
       <div className="row mapAndList">
         <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8" >
+          [//render map]
           <Map userQuery={props.userQuery} selectedActivities={props.selectedActivities} />
         </div>
         <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          [// render activitylist]
           <ActivityList activities={props.activities} />
         </div>
       </div>
@@ -311,7 +364,9 @@ const ResultsPage = props => (
   );
 
 ResultsPage.propTypes = {
+  userQuery: PropTypes.object,
   activities: PropTypes.arrayOf(PropTypes.object),
+  selectedActivities: PropTypes.array,
 };
 
 
@@ -319,13 +374,16 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      // inputs from landing page
       userQuery: {
         budgetOfTrip: 0,
         lengthOfTrip: 0,
         startingLocation: 'honalulu',
         distanceOfTrip: 0,
       },
+      // users interests, generated from clicking interest buttons on landing page
       userInterests: [],
+      // list of results, generated from api call, used by results page
       activities: [],
     };
 
@@ -357,16 +415,12 @@ class App extends Component {
   }
 
   handlePlanButtonClick() {
-    console.log('called');
     axios.get('https://ridb.recreation.gov/api/v1/recareas?apiKey=2CE3A404B8824CFEA7652104FCEEE328&full=TRUE&limit=10')
     .then((res) => {
-      console.log(res.data.RECDATA);
       this.setState({
         activities: generateActivities(res.data.RECDATA),
       });
-    }, console.log(this.state.activities));
-    // send request to google our database to fetch results
-    // route to results page with access to fetched list of results
+    });
   }
 
   render() {
