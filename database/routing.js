7853f46163e5a7a5582289131017a525596d3605
Ryan <ryan@Ryans-MacBook-Pro.local>
2016-12-27 12:57:21 -0600

    DATABASE-BE:Create routing.js file-RS

--- database/routing.js
+++ database/routing.js
@@ -2,8 +2,8 @@ let Sequelize = require('sequelize');
 let schemas = require('../database/schemas.js');
 
 module.exports.getRecOrganization = function(req, res) {
-  let {query: { recArea }} = req;
-  schemas.recAreas.findOne({ where: {RecAreaName: recArea } })
-  .then((recreationArea) => schemas.)
+  // let {query: { recArea }} = req;
+  // schemas.recAreas.findOne({ where: {RecAreaName: recArea } })
+  // .then((recreationArea) => schemas.)
 };
 
