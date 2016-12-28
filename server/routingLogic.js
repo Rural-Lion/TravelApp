dc7e6e4f8af90ae70a069b5a63a795d63a471bcc
Ryan <ryan@Ryans-MacBook-Pro.local>
2016-12-28 02:26:41 -0600

    DATABASE-BE:setting up backend routing-RS

--- server/routingLogic.js
+++ server/routingLogic.js
@@ -2,8 +2,13 @@ let Sequelize = require('sequelize');
 let schemas = require('../database/schemas.js');
 
 module.exports.getRecOrganization = function(req, res) {
-  let {query: { recArea }} = req;
-  schemas.recAreas.findOne({ where: {RecAreaName: recArea } })
-  .then((recreationArea) => recreationArea.getRecOrganization)
-  .then((organization) => res.send(organization));
+  // let {query: { recArea }} = req;
+  // schemas.recAreas.findOne({ 
+  //   where: {RecAreaName: recArea }, 
+  //   include: [{
+  //     model: schemas.organization
+  //   }]
+  // }).then((organization) => res.send(organization))
+  // .catch((err) => console.log('error', err));
 };
+
