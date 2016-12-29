3140e26b0039c070a40bbbec4fbdfc6327b1c4ae
Iana Andrychowicz <filonenko.jana@gmail.com>
2016-12-29 19:45:24 +0200

    GOOGLE_MAPS-FE: refactor to use js api with componentDidMount-IA

--- client/app/ResultsPage/Map.jsx
+++ client/app/ResultsPage/Map.jsx
@@ -1,29 +1,50 @@
 import React, { Component, PropTypes } from 'react';
 import {FancyBorder} from '../helpers.js';
 
-// INCOMPLETE
-  // has no interaction with user inputs or events
-const Map = props => {
-  console.log("MAP props", props);
-  let destination = props.selectedActivities.length ? props.selectedActivities[0].name + props.selectedActivities[0].address : "Yosemite";
-  return (
-    <div>
-      <div className="row">
-        <h1 className="text-center">Results Map</h1>
+class Map extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+  componentDidMount() {
+    this.map = new google.maps.Map(this.refs.map, {
+      center: {lat: 37.775, lng: -122.419},
+      zoom: 10
+    });
+  }
+  //GOOGLE MAPS JS API:
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <h1 className="text-center">Results Map</h1>
+        </div>
+        <div className="row mapContainer" ref="map">
+        </div>
       </div>
-      <div className="row mapContainer">
-        <iframe
-          frameBorder="0"
-          src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyCBYpJtIs3LND6eRzsNbPldwx9cqHh3WOM&origin=
-            ${props.userQuery.startingLocation}&destination=${destination}`}
-          allowFullScreen
-          className="map"
-        />
+    )
+  }
+  
+  //GOOGLE MAPS EMBED API:
+  // let destination: props.selectedActivities.length ? props.selectedActivities[0].name + props.selectedActivities[0].address : "Yosemite",
+  // return (
+  //   <div>
+  //     <div className="row">
+  //       <h1 className="text-center">Results Map</h1>
+  //     </div>
+  //     <div className="row mapContainer">
+  //       <iframe
+  //         frameBorder="0"
+  //         src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyCBYpJtIs3LND6eRzsNbPldwx9cqHh3WOM&origin=
+  //           ${props.userQuery.startingLocation}&destination=${destination}`}
+  //         allowFullScreen
+  //         className="map"
+  //       />
 
-      </div>
-    </div>
-  );
-}
+  //     </div>
+  //   </div>
+  // );
+};
 Map.propTypes = {
   userQuery: PropTypes.object,
   selectedActivities: PropTypes.array,
