6be6119d2c9fb7b0908b2fe283f171ffd2ed8a37
Iana Andrychowicz <filonenko.jana@gmail.com>
2016-12-31 19:19:19 +0200

    MAP-FE: add marker clusters for activities array-IA

--- client/app/ResultsPage/Map.jsx
+++ client/app/ResultsPage/Map.jsx
@@ -10,37 +10,57 @@ class Map extends Component {
   }
    //GOOGLE MAPS JS API:
   initMap(ref) {
-    console.log("initMap called")
-    var geocoder = new google.maps.Geocoder();
+    let map, markerClusterer;
+    if (markerClusterer) {
+          markerClusterer.clearMarkers();
+        }
+    const geocoder = new google.maps.Geocoder();
+    const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let props = this.props;
+    
     geocoder.geocode({'address': this.props.userQuery.startingLocation}, function(results, status) {
-      console.log("GEOCODING", results, status);
+      let locations = props.selectedActivities.map(
+      (item) => {return {lat: item.coordinates[1], lng: item.coordinates[0]}});
       if (status === 'OK') {
-        console.log("status OK")
-        this.map = new google.maps.Map(ref, {
+        map = new google.maps.Map(ref, {
           center: results[0].geometry.location,
-          zoom: 10
+          zoom: 5
+        });
+        var marker = new google.maps.Marker({
+          position: results[0].geometry.location,
+          map: map
         });
       } else {
-        console.log("status not ok")
-        this.map = new google.maps.Map(ref, {
+        map = new google.maps.Map(ref, {
           center: {lat: 37.775, lng: -122.419},
-          zoom: 10
+          zoom: 5
         });
       }
+      var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+          position: location,
+          label: labels[i % labels.length],
+          map: map
+        });
+      });
+      markerClusterer = new MarkerClusterer(map, markers,
+        {imagePath: './maps/img/m'});
     });
-  };
+  }
   componentDidMount() {
     this.initMap(this.refs.map);
   }
  
   render() {
     let query = this.props.userQuery.startingLocation;
-    if (query) this.initMap(this.refs.map);
+    if (this.props.selectedActivities.length || query) {
+      console.log("rerendering")
+      this.initMap(this.refs.map)
+    };
     return (
       <div>
         <div className="row">
           <h1 className="text-center">Results Map</h1>
-          <p>test test test {query}</p>
         </div>
         <div className="row mapContainer" ref="map">
         </div>
