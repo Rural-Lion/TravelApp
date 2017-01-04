8057cf629fdbc29c51d65540e6eef032b2f195c8
Iana Andrychowicz <filonenko.jana@gmail.com>
2017-01-03 16:47:09 -0800

    Routes-FE: add routing back to laning page-IA

--- client/app/ResultsPage/NavBar.jsx
+++ client/app/ResultsPage/NavBar.jsx
@@ -1,12 +1,15 @@
 import React, { Component, PropTypes } from 'react';
 import { FancyBorder } from '../helpers.js';
+import { Link } from 'react-router';
 
 const NavBar = props => (
   <FancyBorder color="yellow">
     <div className="row navBar">
       <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1">
         <FancyBorder color="green">
-          <button type="button" className="goBackButton btn btn-default">Go Back</button>
+          <Link to="/">
+            <button type="button" className="goBackButton btn btn-default">Go Back</button>
+          </Link>
         </FancyBorder>
       </div>
       <div className="col-xs-offset-6 col-sm-offset-6 col-md-offset-6 col-lg-offset-6 col-xs-2 col-sm-2 col-md-2 col-lg-2">
