5d26e58b5bdc55013dd4e6b2880c38431a0a7fec
Romain DEJEAN <romain.dejean@gadz.org>
2017-01-05 15:24:12 -0800

    Test-DB: testing connection to Redis -RD

--- database/database-redis.js
+++ database/database-redis.js
@@ -1,6 +1,6 @@
 require('dotenv-safe').load();
 const redis = require('redis');
-const client = redis.createClient(process.env.REDIS_DB_PORT, process.env.REDIS_DB_ENDPOINT);
+const client = redis.createClient(process.env.REDIS_DB_PORT, 'rurallion1.xzhv3s.ng.0001.usw2.cache.amazonaws.com');
 
 client.on('connect', function() {
   console.log('connected to Redis');
