985bbaeb10f5029782668a4a76b551d3604cfc5e
Romain DEJEAN <romain.dejean@gadz.org>
2017-01-05 15:25:59 -0800

    Fix-DB: environment var - RD

--- database/database-redis.js
+++ database/database-redis.js
@@ -1,6 +1,6 @@
 require('dotenv-safe').load();
 const redis = require('redis');
-const client = redis.createClient(process.env.REDIS_DB_PORT, 'rurallion1.xzhv3s.ng.0001.usw2.cache.amazonaws.com');
+const client = redis.createClient(process.env.REDIS_DB_PORT, process.env.REDIS_DB_ENDPOINT);
 
 client.on('connect', function() {
   console.log('connected to Redis');
