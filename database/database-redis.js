ace95afc45b0c8866a37f99e29fa50f24796d337
Romain DEJEAN <romain.dejean@gadz.org>
2017-01-05 15:06:47 -0800

    Setup-DB: Connection to Amazon Redis - RD

--- database/database-redis.js
+++ database/database-redis.js
@@ -1,6 +1,6 @@
 require('dotenv-safe').load();
 const redis = require('redis');
-const client = redis.createClient();
+const client = redis.createClient(process.env.REDIS_DB_PORT, process.env.REDIS_DB_ENDPOINT);
 
 client.on('connect', function() {
   console.log('connected to Redis');
