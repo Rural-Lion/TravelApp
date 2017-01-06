ac045147d73ef32d891895b9ebb6d4d0ef9e0006
Ryan <ryanpsuresh@gmail.com>
2017-01-05 17:35:43 -0800

    MAPS-FE:display flag on maps-RS

--- client/app/ResultsPage/Map/MapClusterer.js
+++ client/app/ResultsPage/Map/MapClusterer.js
@@ -3,7 +3,7 @@ const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 const MapMarkerClusterer = (entities, map) => {
   if (map) {
-    const markers = entities.map(({ coordinates: [lat, lng] }, index) =>
+    const markers = entities.map(({ coordinates: [lat, lng] }, index) => 
       new google.maps.Marker({
         position: { lat, lng },
         label: labels[index % labels.length],
