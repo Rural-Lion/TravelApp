a18b26bc6d3fe749b52441ad6f69d42777594ad5
Iana Andrychowicz <filonenko.jana@gmail.com>
2017-01-06 17:20:53 -0800

    MODAL-FE: change grid structure to accomodate map-IA

--- client/app/ResultsPage/EntityPopup.jsx
+++ client/app/ResultsPage/EntityPopup.jsx
@@ -10,37 +10,23 @@ const EntityPopup = props => (
     >
       <Modal.Header closeButton>
         <Modal.Title>{props.entity.name}</Modal.Title>
+        <FancyBorder color="blue"><div>Activities: {props.entity.activities.join(', ')}</div></FancyBorder>
       </Modal.Header>
       <Modal.Body className="container">
         <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
         THIS WILL BE THE MAP
         </div>
         <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6 entityModalBody">
-          <div className="row ">
-            <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-            Activities:
+          <FancyBorder color="blue">
+            <div dangerouslySetInnerHTML={{ __html: props.entity.description }} />
+          </FancyBorder>
+        </div>
+        <div className="row">
+          <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-center">
+            <FancyBorder color="blue"><div>{props.entity.phone}</div></FancyBorder>
           </div>
-            <div className="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-              <FancyBorder color="blue"><div>{props.entity.activities.join(', ')}</div></FancyBorder>
-            </div>
-          </div>
-          <div className="row ">
-            <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-            Description:
-          </div>
-            <div className="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-              <FancyBorder color="blue">
-                <div dangerouslySetInnerHTML={{ __html: props.entity.description }} />
-              </FancyBorder>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-center">
-              <FancyBorder color="blue"><div>{props.entity.phone}</div></FancyBorder>
-            </div>
-            <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8 text-center">
-              <FancyBorder color="blue"><div>{props.entity.email}</div></FancyBorder>
-            </div>
+          <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8 text-center">
+            <FancyBorder color="blue"><div>{props.entity.email}</div></FancyBorder>
           </div>
         </div>
       </Modal.Body>
