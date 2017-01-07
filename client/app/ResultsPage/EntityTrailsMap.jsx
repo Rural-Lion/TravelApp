825cc3aae340719e043dd7275da7af987d6dc729
Iana Andrychowicz <filonenko.jana@gmail.com>
2017-01-07 15:24:11 -0800

    TrailMap-FE: pass props, render&center map-IA

--- client/app/ResultsPage/EntityTrailsMap.jsx
+++ client/app/ResultsPage/EntityTrailsMap.jsx
@@ -1,52 +1,63 @@
 import React, { Component, PropTypes } from 'react';
-import { FancyBorder } from '../../helpers';
+import { FancyBorder } from '../helpers';
 import MapClusterer from './Map/MapClusterer';
 
-
-import React, { Component, PropTypes } from 'react';
-
-class componentName extends Component {
+class EntityTrailsMap extends Component {
   constructor(props) {
     super(props);
+    this.state = {};
+    this.map = '';
   }
 
-  componentWillMount() {
+  // componentWillMount() {
 
-  }
+  // }
 
   componentDidMount() {
-
+    this.initMap(this.map);
   }
 
-  componentWillReceiveProps(nextProps) {
+  // componentWillReceiveProps(nextProps) {
 
+  // }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+
+  // }
+
+  // componentWillUpdate(nextProps, nextState) {
+
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+
+  // }
+
+  // componentWillUnmount() {
+
+  // }
+  initMap(mapRef) {
+    console.log('init map called', mapRef);
+    this.setState({
+      map: new google.maps.Map(mapRef, {
+        center: { lat: this.props.center[0], lng: this.props.center[1] },
+        zoom: 9,
+      }),
+    });
   }
-
-  shouldComponentUpdate(nextProps, nextState) {
-
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-
-  }
-
-  componentWillUnmount() {
-
-  }
-
   render() {
     return (
-      <div />
+      <FancyBorder color="green">
+        <div className="modalMap container-fluid" ref={(map) => { this.map = map; }} />
+      </FancyBorder>
     );
   }
 }
 
-componentName.propTypes = {
-
+EntityTrailsMap.propTypes = {
+  center: PropTypes.arrayOf(PropTypes.number),
+  trails: PropTypes.arrayOf(PropTypes.object),
+  entityID: PropTypes.number,
 };
 
-export default componentName;
+export default EntityTrailsMap;
