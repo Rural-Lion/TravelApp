c12ca2789664ef851ec2f4decb1d4331d2cedd11
Romain DEJEAN <romain.dejean@gadz.org>
2017-01-07 16:59:57 -0800

    setup-test: generateData function tested - RD

--- client/spec/helpers/generateData.test.js
+++ client/spec/helpers/generateData.test.js
@@ -18,7 +18,7 @@ describe('generateData', () => {
     expect(typeof generateData(entities)[0]).to.be('object');
   });
 
-  it('Should reformat the recAreas data appropriately', () => {
+  it('Should reformat a recArea data appropriately', () => {
     const entities = [{
       URL: 'https://en.wikipedia.org/wiki/Westworld_(s%C3%A9rie_t%C3%A9l%C3%A9vis%C3%A9e)',
       EntityID: 58,
@@ -41,7 +41,7 @@ describe('generateData', () => {
     expect(generateData(entities)[0].facility).to.be(false);
   });
 
-  it('Should reformat the facilities data appropriately', () => {
+  it('Should reformat a facility data appropriately', () => {
     const entities = [{
       URL: 'https://en.wikipedia.org/wiki/Westworld_(s%C3%A9rie_t%C3%A9l%C3%A9vis%C3%A9e)',
       EntityID: 58,
@@ -63,6 +63,54 @@ describe('generateData', () => {
     expect(generateData(entities)[0].entityID).to.be(58);
     expect(generateData(entities)[0].facility).to.be(true);
   });
+
+  it('Should reformat all recAreas data appropriately', () => {
+    const entities = [{
+      URL: 'https://en.wikipedia.org/wiki/Westworld_(s%C3%A9rie_t%C3%A9l%C3%A9vis%C3%A9e)',
+      EntityID: 58,
+      RecAreaLatitude: 30.61,
+      RecAreaLongitude: -96.33,
+      RecAreaName: 'WestWorld',
+      RecAreaPhone: '760-557-9875',
+      RecAreaDescription: 'WestWorld is located in north Scottsdale at the base of the McDowell Mountains.',
+      RecAreaEmail: 'westworld@gmail.com'
+    },
+    {
+      URL: 'https://en.wikipedia.org/wiki/Westworld2_(s%C3%A9rie_t%C3%A9l%C3%A9vis%C3%A9e)',
+      EntityID: 59,
+      RecAreaLatitude: 30.62,
+      RecAreaLongitude: -96.34,
+      RecAreaName: 'WestWorld2',
+      RecAreaPhone: '760-557-9876',
+      RecAreaDescription: 'WestWorld2 is located in north Scottsdale at the base of the McDowell Mountains.',
+      RecAreaEmail: 'westworld2@gmail.com'
+    }];
+    //Testing first entity in the array of results
+    expect(generateData(entities)[0].name).to.be('WestWorld');
+    expect(generateData(entities)[0].image).to.be('https://en.wikipedia.org/wiki/Westworld_(s%C3%A9rie_t%C3%A9l%C3%A9vis%C3%A9e)');
+    expect(generateData(entities)[0].email).to.be('westworld@gmail.com');
+    expect(generateData(entities)[0].phoneNumber).to.be('760-557-9875');
+    expect(generateData(entities)[0].description).to.be('WestWorld is located in north Scottsdale at the base of the McDowell Mountains.');
+    expect(generateData(entities)[0].coordinates[0]).to.be(30.61);
+    expect(generateData(entities)[0].coordinates[1]).to.be(-96.33);
+    expect(generateData(entities)[0].recArea).to.be(true);
+    expect(generateData(entities)[0].entityID).to.be(58);
+    expect(generateData(entities)[0].facility).to.be(false);
+
+    //Testing second entity in the array of results
+    expect(generateData(entities)[1].name).to.be('WestWorld2');
+    expect(generateData(entities)[1].image).to.be('https://en.wikipedia.org/wiki/Westworld2_(s%C3%A9rie_t%C3%A9l%C3%A9vis%C3%A9e)');
+    expect(generateData(entities)[1].email).to.be('westworld2@gmail.com');
+    expect(generateData(entities)[1].phoneNumber).to.be('760-557-9876');
+    expect(generateData(entities)[1].description).to.be('WestWorld2 is located in north Scottsdale at the base of the McDowell Mountains.');
+    expect(generateData(entities)[1].coordinates[0]).to.be(30.62);
+    expect(generateData(entities)[1].coordinates[1]).to.be(-96.34);
+    expect(generateData(entities)[1].recArea).to.be(true);
+    expect(generateData(entities)[1].entityID).to.be(59);
+    expect(generateData(entities)[1].facility).to.be(false);
+  });
+
+
 });
 
 
