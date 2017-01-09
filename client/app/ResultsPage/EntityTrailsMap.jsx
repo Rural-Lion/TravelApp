bf81c872899864f9176d33d7afe3a375c3c60121
Iana Andrychowicz <filonenko.jana@gmail.com>
2017-01-09 11:24:44 -0800

    TrailsMap-FE: show/hide polylines on click-IA

--- client/app/ResultsPage/EntityTrailsMap.jsx
+++ client/app/ResultsPage/EntityTrailsMap.jsx
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react';
 import { FancyBorder } from '../helpers';
-import MapMarkerClusterer from './Map/MapClusterer';
 
 class EntityTrailsMap extends Component {
   constructor(props) {
@@ -15,11 +14,10 @@ class EntityTrailsMap extends Component {
   componentWillMount() {
     this.setState({
       trails: this.parseTrails(),
-    }, () => { console.log('set state to parsed trails', this.state.trails); });
+    });
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log('comparing state:', this.state, nextState);
     if (!this.state.mapRef) {
       return true;
     }
@@ -35,33 +33,39 @@ class EntityTrailsMap extends Component {
       }, () => { this.createTrailMarkers(this.state.map); });
     });
   }
+
   initMap(mapRef) {
-    console.log('here are the trails', this.state.trails);
+    // create a map and link it to a dom element
     const map = new google.maps.Map(mapRef, {
       center: { lat: this.props.center[0], lng: this.props.center[1] },
-      zoom: 9,
+      zoom: 8,
       mapTypeId: 'terrain',
     });
+    // make a marker for the entity
     const entityMarker = new google.maps.Marker({
       position: { lat: this.props.center[0], lng: this.props.center[1] },
       map,
       animation: google.maps.Animation.DROP,
     });
+    // make an infowindow for the entity
     const infoWindow = new google.maps.InfoWindow({
       content: this.props.entityName,
     });
-
+    // add an event listener to open the infowindow on click
     entityMarker.addListener('click', () => {
       infoWindow.open(map, entityMarker);
     });
+    // open the infowindow on render
     infoWindow.open(map, entityMarker);
     return map;
   }
+
   parseTrails() {
+    // get the trail coordinates in the right format
     return this.props.trails.map((trail) => {
       trail.coordinates = trail.coordinates
-        .slice(12, trail.coordinates.length - 2)
-        .split(',')
+        .slice(12, trail.coordinates.length - 1)
+        .split(', ')
         .map((point) => {
           point = point.split(' ');
           return { lat: +point[1], lng: +point[0] };
@@ -69,28 +73,39 @@ class EntityTrailsMap extends Component {
       return trail;
     });
   }
+
   createTrailMarkers(map) {
     const infoWindow = new google.maps.InfoWindow();
     const trailMarkers = this.state.trails.map((trail) => {
-      console.log('making trail markers on', map);
+      // create a marker for each trail:
       const marker = new google.maps.Marker({
         position: trail.coordinates[0],
         icon: './maps/icons/hiking2.png',
         map,
         title: trail.name,
       });
+      // add an event listener for each marker to open an infowindow:
       marker.addListener('mouseover', () => {
         infoWindow.setContent(`${trail.name}: ${trail.length}m`);
         infoWindow.open(map, marker);
       });
+      // create a polyline for each trail to draw it on the map:
+      const path = this.createPolyline(trail);
+      let showPath = true;
+      // add an event listener that shows/hides the trail when clicked:
       marker.addListener('click', () => {
-        this.createPolyline(trail, map);
+        showPath ? path.setMap(map) : path.setMap(null);
+        showPath = !showPath;
       });
+
       return marker;
     });
+    // show trail markers in clusters:
     return new MarkerClusterer(map, trailMarkers, { imagePath: './maps/img/m' });
   }
-  createPolyline(trail, map) {
+
+  createPolyline(trail) {
+    // create a polyline object
     const trailPath = new google.maps.Polyline({
       path: trail.coordinates,
       geodesic: true,
@@ -98,8 +113,9 @@ class EntityTrailsMap extends Component {
       strokeOpacity: 1.0,
       strokeWeight: 2,
     });
-    trailPath.setMap(map);
+    return trailPath;
   }
+
   render() {
     return (
       <FancyBorder color="green">
