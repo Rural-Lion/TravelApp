b01edc551e9d41ed348cb047ef61ecce28eb124f
Ryan <ryanpsuresh@gmail.com>
2017-01-10 16:24:04 -0800

    OPTIONS-FE:create plan button-RS

--- client/app/ResultsPage/Options/Inputs.jsx
+++ client/app/ResultsPage/Options/Inputs.jsx
@@ -2,13 +2,14 @@ import React, { Component, PropTypes } from 'react';
 import { FancyBorder } from '../../helpers.js';
 import { setLocation, setBudget, setDistance, setLength } from '../../actions/inputActions.js';
 import Autocomplete from 'react-google-autocomplete';
+import PlanVacationButton from './PlanVacationButton.jsx';
 
 const Inputs = props => (
   <FancyBorder color="yellow" >
-    <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+    <div className='optionsInput'>
       <FancyBorder color="green">
       $$$
-        <input
+        <input style={{width: '350px'}}
           data-tag="budgetOfTrip"
           onChange={e => props.handleChange(setBudget, e.target.value)}
           className="lpInput"
@@ -17,10 +18,10 @@ const Inputs = props => (
         />
       </FancyBorder>
     </div>
-    <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+    <div className='optionsInput'>
       <FancyBorder color="green">
       Time
-        <input
+        <input style={{width: '350px'}}
           data-tag="lengthOfTrip"
           onChange={e => props.handleChange(setLength, e.target.value)}
           className="lpInput"
@@ -29,10 +30,10 @@ const Inputs = props => (
         />
       </FancyBorder>
     </div>
-    <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+    <div className='optionsInput'>
       <FancyBorder color="green">
       Location
-        <Autocomplete
+        <Autocomplete style={{width: '350px'}}
           data-tag="startingLocation"
           onPlaceSelected={place => props.handleChange(setLocation, place.formatted_address)}
           onChange={e => props.handleChange(setLocation, e.target.value)}
@@ -42,10 +43,10 @@ const Inputs = props => (
         />
       </FancyBorder>
     </div>
-    <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+    <div className='optionsInput'>
       <FancyBorder color="green">
       Distance
-        <input
+        <input style={{width: '350px'}}
           data-tag="distanceOfTrip"
           onChange={e => props.handleChange(setDistance, e.target.value)}
           className="lpInput"
@@ -53,6 +54,56 @@ const Inputs = props => (
           value={props.userQuery.distanceOfTrip}
         />
       </FancyBorder>
+    </div>
+    <div className='optionsInput'>
+    Day Length
+      <FancyBorder  color="green" >
+        Wake Up
+        <input style={{width: '150px'}}
+          data-tag="dayLength1"
+         /*onChange={e => props.handleChange(setDistance, e.target.value)}*/
+          className="lpInput"
+          type="text"
+          /*value={props.userQuery.dayLength}*/
+        />
+        Come Back to Camp
+        <input style={{width: '150px'}}
+          data-tag="dayLength2"
+         /*onChange={e => props.handleChange(setDistance, e.target.value)}*/
+          className="lpInput"
+          type="text"
+          /*value={props.userQuery.dayLength}*/
+        />
+      </FancyBorder>
+    </div>
+    <div className='optionsInput'>
+      <FancyBorder color="green">
+      Daily Allowance for Food
+        <input style={{width: '350px'}}
+          data-tag="allowanceForFood"
+          /* onChange={e => props.handleChange(setDistance, e.target.value)} */
+          className="lpInput"
+          type="text"
+          /* value={props.userQuery.distanceOfTrip} */
+        />
+      </FancyBorder>
+    </div>
+    <div className='optionsInput'>
+      <FancyBorder color="green">
+      Sleeping Preferences
+        <input style={{width: '350px'}}
+          data-tag="sleep preference"
+          /* onChange={e => props.handleChange(setDistance, e.target.value)} */
+          className="lpInput"
+          type="text"
+          /* value={props.userQuery.distanceOfTrip} */
+        />
+      </FancyBorder>
+    </div>
+    <div className="row">
+      <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-centered">
+        <PlanVacationButton />
+      </div>
     </div>
   </FancyBorder>
   );
---
+++ client/app/ResultsPage/Options/PlanVacationButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FancyBorder } from '../../helpers.js';
+import { Link } from 'react-router';
+
+const PlanVacationButton = () => (
+  <FancyBorder color="yellow">
+    <Link to="/results">
+      <button className="centered planVacationButton">Plan My Vacation</button>
+    </Link>
+  </FancyBorder>
+);
+
+export default PlanVacationButton;
