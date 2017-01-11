f5a8f549231d2c7d088ad67ba123007d857c1358
Romain DEJEAN <romain.dejean@gadz.org>
2017-01-11 00:18:59 -0800

    Setup-test: Trails&Act. per facility tested - RD

--- server/Spec/trailsAndActivitiesWithinRadiusOfFacility.test.js
+++ server/Spec/trailsAndActivitiesWithinRadiusOfFacility.test.js
@@ -6,7 +6,7 @@ chai.use(chaiHttp);
 
 describe('trailsAndActivitiesWithinRadiusOfFacility', function() {
   this.timeout(30000);
-  it ('Should return trails and activities around a facility on GET', function(done) {
+  it ('Should return trails and activities around a facility on GET for longitude < -100', function(done) {
     chai.request('localhost:8000')
     .get('/trailsAndActivitiesWithinRadiusOfFacility')
     .query({latitude: 47.2487, longitude: -120.6776, facilityID: 201792})
@@ -18,7 +18,7 @@ describe('trailsAndActivitiesWithinRadiusOfFacility', function() {
     });
   });
 
-  it ('Should return the adequate entity\'s properties', function(done) {
+  it ('Should return the adequate entity\'s properties  for longitude < -100', function(done) {
     chai.request('localhost:8000')
     .get('/trailsAndActivitiesWithinRadiusOfFacility')
     .query({latitude: 47.2487, longitude: -120.6776, facilityID: 201792})
@@ -35,13 +35,53 @@ describe('trailsAndActivitiesWithinRadiusOfFacility', function() {
     });
   });
 
-  it ('Should return the correct trails and activities', function(done) {
+  it ('Should return the correct trails and activities  for longitude < -100', function(done) {
     chai.request('localhost:8000')
     .get('/trailsAndActivitiesWithinRadiusOfFacility')
     .query({latitude: 47.2487, longitude: -120.6776, facilityID: 201792})
     .end(function(err, res){
       res.body.trails.length.should.equal(366); 
       res.body.activities.length.should.equal(2); 
+      done();
+    });
+  });
+
+  it ('Should return trails and activities around a facility on GET for longitude > -100', function(done) {
+    chai.request('localhost:8000')
+    .get('/trailsAndActivitiesWithinRadiusOfFacility')
+    .query({latitude: 43.8333, longitude: -72.6345, facilityID: 203842})
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      done();
+    });
+  });
+
+  it ('Should return the adequate entity\'s properties for longitude > -100', function(done) {
+    chai.request('localhost:8000')
+    .get('/trailsAndActivitiesWithinRadiusOfFacility')
+    .query({latitude: 43.8333, longitude: -72.6345, facilityID: 203842})
+    .end(function(err, res){
+      res.body.should.have.property('trails');
+      res.body.trails.should.be.a('array');
+      res.body.should.have.property('activities');
+      res.body.activities.should.be.a('array');
+      res.body.trails[0].should.have.property('id');
+      res.body.trails[0].should.have.property('name');
+      res.body.trails[0].should.have.property('length');
+      res.body.trails[0].should.have.property('coordinates');
+      done();
+    });
+  });
+
+  it ('Should return the correct trails and activities for longitude > -100', function(done) {
+    chai.request('localhost:8000')
+    .get('/trailsAndActivitiesWithinRadiusOfFacility')
+    .query({latitude: 43.8333, longitude: -72.6345, facilityID: 203842})
+    .end(function(err, res){
+      res.body.trails.length.should.equal(282); 
+      res.body.activities.length.should.equal(3); 
       done();
     });
   });
