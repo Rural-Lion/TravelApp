2a0c07c7c143309326311b73cd918dbe7aa2294f
Romain DEJEAN <romain.dejean@gadz.org>
2017-01-11 08:49:08 -0800

    Setup-test: GetEntitieswithinradius tested - RD

--- server/Spec/getEntitiesWithinRadius.test.js
+++ server/Spec/getEntitiesWithinRadius.test.js
@@ -5,11 +5,11 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('getEntitiesWithinRadius', function() {
-  this.timeout(30000);
+  this.timeout(5000);
   it ('Should return an array of entities on GET', function(done) {
     chai.request('localhost:8000')
     .get('/entitiesWithinRadius')
-    .query({latitude: 39.742043, longitude: -104.991531, distance: 50, activities: ["\'BOATING\'"]})
+    .query({latitude: 39.742043, longitude: -104.991531, distance: 50, activities: JSON.stringify(['BOATING'])})
     .end(function(err, res){
       res.should.have.status(200);
       res.should.be.json;
@@ -18,39 +18,38 @@ describe('getEntitiesWithinRadius', function() {
     });
   });
 
-  // it ('Should return the adequate entity\'s properties', function(done) {
-    // chai.request('localhost:8000')
-  //   .get('/entitiesWithinRadius')
-  //   .query({latitude: 39.742043, longitude: -104.991531, distance: 200, activities: ['BOATING']})
-  //   .end(function(err, res){
-  //     res.body[0].should.have.property('FacilityLatitude');
-  //     res.body[0].should.have.property('FacilityLongitude');
-  //     res.body[0].should.have.property('FacilityName');
-  //     res.body[0].should.have.property('FacilityPhone');
-  //     res.body[0].should.have.property('FacilityDescription');
-  //     res.body[0].should.have.property('FacilityEmail');
-  //     res.body[0].should.have.property('RecAreaName');
-  //     res.body[0].should.have.property('RecAreaLatitude');
-  //     res.body[0].should.have.property('RecAreaLongitude');
-  //     res.body[0].should.have.property('RecAreaPhone');
-  //     res.body[0].should.have.property('RecAreaDescription');
-  //     res.body[0].should.have.property('RecAreaEmail');
-  //     res.body[0].should.have.property('URL');
-  //     res.body[0].should.have.property('EntityID');
-  //     res.body[0].should.have.property('EntityType');
-  //     res.body[0].should.have.property('ActivityDescription');
-  //     done();
-  //   });
-  // });
+  it ('Should return the adequate entity\'s properties', function(done) {
+    chai.request('localhost:8000')
+    .get('/entitiesWithinRadius')
+    .query({latitude: 39.742043, longitude: -104.991531, distance: 50, activities: JSON.stringify(['BOATING'])})
+    .end(function(err, res){
+      res.body[0].should.have.property('FacilityLatitude');
+      res.body[0].should.have.property('FacilityLongitude');
+      res.body[0].should.have.property('FacilityName');
+      res.body[0].should.have.property('FacilityPhone');
+      res.body[0].should.have.property('FacilityDescription');
+      res.body[0].should.have.property('FacilityEmail');
+      res.body[0].should.have.property('RecAreaName');
+      res.body[0].should.have.property('RecAreaLatitude');
+      res.body[0].should.have.property('RecAreaLongitude');
+      res.body[0].should.have.property('RecAreaPhone');
+      res.body[0].should.have.property('RecAreaDescription');
+      res.body[0].should.have.property('RecAreaEmail');
+      res.body[0].should.have.property('URL');
+      res.body[0].should.have.property('EntityID');
+      res.body[0].should.have.property('EntityType');
+      res.body[0].should.have.property('ActivityDescription');
+      done();
+    });
+  });
 
-  // it ('Should return the correct Facility address', function(done) {
-  //   chai.request('localhost:8000')
-  //   .get('/entitiesWithinRadius')
-  //   .query({latitude: 39.742043, longitude: -104.991531, distance: 200, activities: ['BOATING']})
-  //   .end(function(err, res){
-  //     res.body.length.should.equal(50); 
-  //     done();
-  //   });
-  // });
-
+  it ('Should return the correct Facility address', function(done) {
+    chai.request('localhost:8000')
+    .get('/entitiesWithinRadius')
+    .query({latitude: 39.742043, longitude: -104.991531, distance: 50, activities: JSON.stringify(['BOATING'])})
+    .end(function(err, res){
+      res.body.length.should.equal(6); 
+      done();
+    });
+  });
 });
