e0ab63e25faf9da4fe848c9028c95f3e52d3d1ab
Romain DEJEAN <romain.dejean@gadz.org>
2017-01-11 11:47:46 -0800

    Del-BE: Deleted unused routes - RD

--- server/routing.js
+++ server/routing.js
@@ -27,36 +27,16 @@ module.exports = function (app, express) {
   app.get('/recArea', routeLogic.getRecArea);
   app.get('/facility', routeLogic.getFacility);
 
-  // Get Organizations for RecAreas and Facilities
-  app.get('/recAreaOrg', routeLogic.getRecOrganization);
-  app.get('/facilityOrg', routeLogic.getFacilityOrganization);
-
   // Get Activities for RecAreas and Facilities
   app.get('/recActivities', routeLogic.getRecActivities);
   app.get('/facilityActivities', routeLogic.getFacilitiesActivities);
-
-// Get EntityLinks for RecAreas and Facilities
-  app.get('/recLinks', routeLogic.getRecLinks);
-  app.get('/facilityLinks', routeLogic.getFacilityLinks);
 
   // Get EntityMedia for RecAreas and Facilities
   app.get('/recMedia', routeLogic.getRecMedia);
   app.get('/facilityMedia', routeLogic.getFacilityMedia);
 
-  // Get Tours for Facilities
-  app.get('/facilityTours', routeLogic.getFacilityTours);
-
-  // Get Facility Campsites
-  app.get('/facilityCampsites', routeLogic.getFacilityCampsites);
-
-  // Get Facility Permit Entrances
-  app.get('/facilityEntrances', routeLogic.getFacilityPermitEntrances);
-
   // Get activities for RecArea and Facility
   app.get('/activities', routeLogic.getActivities);
-
-  // Get permitEntrances for RecArea and Facility
-  app.get('/permitEntrances', routeLogic.getEntrances);
 
 /////////////////////////////////////////////////////////////
 };
