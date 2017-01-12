52d4b6f9567947f04c3088c7539724003ef3498f
Ryan <ryanpsuresh@gmail.com>
2017-01-12 15:16:55 -0800

    REDUX-FE:group interests-RS

--- client/app/reducers/interestsReducer.js
+++ client/app/reducers/interestsReducer.js
@@ -1,14 +1,14 @@
-const initialState = [
-  ['Biking', false], ['Boating', false], ['Historic & Cultural Site', false], ['Camping', false], ['Fishing', false],
-  ['Hiking', true], ['Off Highway Vehicle', false], ['Picnicking', false], ['Recreational Vehicles', false],
-  ['Visitor Center', false], ['Water Sports', false], ['Wildlife Viewing', false], ['Other Recreation Concession Site', false],
-];
+// const initialState = [
+//   ['Biking', false], ['Boating', false], ['Historic & Cultural Site', false], ['Camping', false], ['Fishing', false],
+//   ['Hiking', true], ['Off Highway Vehicle', false], ['Picnicking', false], ['Recreational Vehicles', false],
+//   ['Visitor Center', false], ['Water Sports', false], ['Wildlife Viewing', false], ['Other Recreation Concession Site', false],
+// ];
 
 const initialState = [
-  ['Biking', false, ['BIKING']], ['Boating', false, ['BOATING']], ['Cultural', false, ['HISTORIC & CULTURAL SITE', 'ENVIRONMENTAL EDUCATION']], 
-  ['Camping', false, ['PICNICKING', 'CAMPING', 'FIRE LOOKOUTS/CABINS OVERNIGHT', 'HORSE CAMPING']], ['Fishing', false, ['FISHING', 'BOATING']],
+  ['Biking', false, ['BIKING']], ['Fishing', false, ['FISHING', 'BOATING']], ['Cultural Activities', false, ['HISTORIC & CULTURAL SITE', 'ENVIRONMENTAL EDUCATION']], 
+  ['Camping', false, ['PICNICKING', 'CAMPING', 'FIRE LOOKOUTS/CABINS OVERNIGHT', 'HORSE CAMPING']], ['Climbing', false, ['CLIMBING']],
   ['Hiking', true, ['HIKING']], ['Winter Sports', false, ['SNOWPARK', 'WINTER SPORTS']], ['Equestrian', false, ['HORSEBACK RIDING', 'HORSE CAMPING']],
-  ['Photography', false, ['PHOTOGRAPHY', 'OBSERVATION SITE']], ['Water Activities', false, ['BOATING', 'WATER SPORTS','SWIMMING SITE', 'PADDLING', 'SWIMMING', 'DIVING', 'SNORKELING']], 
+  ['Photography', false, ['PHOTOGRAPHY', 'OBSERVATION SITE']], ['Water Sports', false, ['BOATING', 'WATER SPORTS','SWIMMING SITE', 'PADDLING', 'SWIMMING', 'DIVING', 'SNORKELING']], 
   ['Wildlife Viewing', false, ['FISH VIEWING SITE', 'WILDERNESS', 'WILDLIFE VIEWING', 'HUNTING']], ['Motor Sports', false, ['AUTO TOURING', 'OFF HIGHWAY VEHICLE', 'RECREATIONAL VEHICLES']],
 ];
 
@@ -19,7 +19,7 @@ const interests = (state = initialState, action) => {
         if (index !== action.interestId) {
           return interest;
         }
-        return [interest[0], !interest[1]];
+        return [interest[0], !interest[1], interest[2]];
       });
     default:
       return state;
