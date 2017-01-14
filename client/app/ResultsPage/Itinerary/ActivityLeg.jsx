f1773ecb6714280b7e0060ab97ba66ff07faabe5
elliottabirch <elliottabirch@gmail.com>
2017-01-13 18:43:57 -0800

    fix stuff

--- client/app/ResultsPage/Itinerary/ActivityLeg.jsx
+++ client/app/ResultsPage/Itinerary/ActivityLeg.jsx
@@ -67,7 +67,7 @@ class ActivityLeg extends Component {
                     <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                       <div className="row">
                         <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1 stepMarker">
-                          <img className="stepMarker" src="http://www.iconsdb.com/icons/preview/guacamole-green/car-xl.png" />
+                          <img className="stepMarker" src="https://www.iconsdb.com/icons/preview/guacamole-green/car-xl.png" />
                         </div>
                         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 legText startText">{`Start Drive to ${this.state.name} ${this.props.activity.time.startTime[0]}:${this.props.activity.time.startTime[1]}`}</div>
                       </div>
@@ -79,9 +79,9 @@ class ActivityLeg extends Component {
                         <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1 stepMarker duration" />
                         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 legText endText">
                           <div className="row">
-                              <div className="col-xs-7 col-sm-7 col-md-7 col-lg-7 durationText">{`Duration ${durationText}`}</div>
-                              <div>{this.state.activity.start_address ? <button className=" glyphicon glyphicon-triangle-bottom toggleStepButton" onClick={() => this.toggleSteps()} /> : null}</div>
-                            </div>
+                            <div className="col-xs-7 col-sm-7 col-md-7 col-lg-7 durationText">{`Duration ${durationText}`}</div>
+                            <div>{this.state.activity.start_address ? <button className=" glyphicon glyphicon-triangle-bottom toggleStepButton" onClick={() => this.toggleSteps()} /> : null}</div>
+                          </div>
                           {this.state.showSteps ? <Steps steps={this.state.activity.legs} /> : null}
                         </div>
                       </div>
@@ -91,7 +91,7 @@ class ActivityLeg extends Component {
                     <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                       <div className="row">
                         <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2 stepMarker">
-                          <img className="stepMarker" src="http://www.iconsdb.com/icons/preview/soylent-red/car-xl.png" />
+                          <img className="stepMarker" src="https://www.iconsdb.com/icons/preview/soylent-red/car-xl.png" />
                         </div>
                         <div className="col-xs-10 col-sm-10 col-md-10 col-lg-10 legText durationText">{`End Drive to ${this.state.name} ${this.props.activity.time.endTime[0]}:${this.props.activity.time.endTime[1]}`}</div>
                       </div>
@@ -102,7 +102,7 @@ class ActivityLeg extends Component {
                   <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                     <div className="row">
                       <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1 stepMarker">
-                        <img className="stepMarker" src="http://www.iconsdb.com/icons/preview/caribbean-blue/map-marker-2-xl.png" />
+                        <img className="stepMarker" src="https://www.iconsdb.com/icons/preview/caribbean-blue/map-marker-2-xl.png" />
                       </div>
                       <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 legText">{`Stay at ${this.state.name}`} <input className="legHourInput" onChange={(e) => { this.setTimeInput(this.props.activity.name, (+e.target.value * 3600)); }} value={Math.floor(this.state.activity.duration / 3600)} /> hours </div>
                     </div>
--- client/app/ResultsPage/Itinerary/ItineraryContainer.jsx
+++ client/app/ResultsPage/Itinerary/ItineraryContainer.jsx
@@ -5,8 +5,8 @@ import DayLegs from './DayLegs.jsx';
 class ItineraryContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
-    
+    this.state = {};
+
     this.setItineraryDom = this.setItineraryDom.bind(this);
   }
 
@@ -18,20 +18,20 @@ class ItineraryContainer extends Component {
     }
   }
 
-   setItineraryDom(node) {
-    if (node !== this.state.itineraryDom) {
+  setItineraryDom(node) {
+     if (node !== this.state.itineraryDom) {
       this.setState({
-        itineraryDom: node
+        itineraryDom: node,
       });
     }
-  }
-  
+   }
+
   render() {
     console.log('itinerary in itinerarycontainer: ', this.props.itinerary);
     return (
-      <div ref={(node) => {this.props.setItineraryDom(node); this.setItineraryDom(node)}}>
+      <div ref={(node) => { this.props.setItineraryDom(node); this.setItineraryDom(node) ;}}>
         <FancyBorder color="yellow">
-          <DayLegs itinerary={this.props.itinerary} addTimeToWaypoint={this.props.addTimeToWaypoint} />
+          {this.props.itinerary ? <DayLegs itinerary={this.props.itinerary} addTimeToWaypoint={this.props.addTimeToWaypoint} /> : null }
         </FancyBorder>
       </div>
     );
