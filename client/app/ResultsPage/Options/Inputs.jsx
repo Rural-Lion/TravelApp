7f5edbae9d4a30643b7c0652b6d47a16c42c012d
Ryan <ryanpsuresh@gmail.com>
2017-01-13 17:31:26 -0800

    OPTIONS-FE:add bootstrap for options tab-RS

--- client/app/ResultsPage/Options/Inputs.jsx
+++ client/app/ResultsPage/Options/Inputs.jsx
@@ -2,11 +2,13 @@ import React, { Component, PropTypes } from 'react';
 import { FancyBorder } from '../../helpers.js';
 import { setLocation, setBudget, setDistance, setLength } from '../../actions/inputActions.js';
 import Autocomplete from 'react-google-autocomplete';
+import { Button } from 'react-bootstrap'
 
 const Inputs = props => (
-  <div >
-    <div className='optionsInput'>
-      Budget $$$
+  <div className='Optionscontainer'>
+    <div className='row'>
+      <div className='col optionsInput'>
+      <strong>Budget $$$</strong>
         <input id='budget' 
           data-tag="budgetOfTrip"
           className="lpInput"
@@ -14,9 +16,9 @@ const Inputs = props => (
           onChange={(e) => value=e.target.value}
           value={props.userQuery.budgetOfTrip}
         />
-    </div>
-    <div className='optionsInput'>
-      Time Length (Days)
+      </div>
+      <div className='col optionsInput'>
+      <strong>Time Length (Days)</strong>
         <input id='time'
           data-tag="lengthOfTrip"
           className="lpInput"
@@ -24,79 +26,88 @@ const Inputs = props => (
           onChange={(e) => value=e.target.value}
           value={props.userQuery.lengthOfTrip}
         />
+      </div>
     </div>
-    <div className='optionsInput'>
-      Location
-        <Autocomplete id='location' 
-          data-tag="startingLocation"
-          className="lpInput"
-          type="text"
-          onChange={(e) => value=e.target.value}
-          value={props.userQuery.startingLocation}
-        />
+    <div className='row'>
+      <div className='col optionsInput'>
+        <strong>Location</strong>
+          <Autocomplete id='location' 
+            data-tag="startingLocation"
+            className="lpInput"
+            type="text"
+            onChange={(e) => value=e.target.value}
+            value={props.userQuery.startingLocation}
+          />
+      </div>
+      <div className='col optionsInput'>
+        <strong>Distance</strong>
+          <input id='distance' 
+            data-tag="distanceOfTrip"
+            className="lpInput"
+            type="text"
+            onChange={(e) => value=e.target.value}
+            value={props.userQuery.distanceOfTrip}
+          />
+      </div>
     </div>
-    <div className='optionsInput'>
-      Distance
-        <input id='distance' 
-          data-tag="distanceOfTrip"
-          className="lpInput"
-          type="text"
-          onChange={(e) => value=e.target.value}
-          value={props.userQuery.distanceOfTrip}
-        />
+    <div className='row'>
+      <div className='col optionsInput'>
+          <strong>Wake Up</strong>
+          <input id='startingTime' 
+            data-tag="dayLength1"
+            className="lpInput"
+            type="text"
+            onChange={(e) => value=e.target.value}
+            value={props.startingTime}
+          />
+      </div>
+      <div className='col optionsInput'>
+          <strong>Come Back to Camp</strong>
+          <input id='endingTime' 
+            data-tag="dayLength2"
+            className="lpInput"
+            type="text"
+            onChange={(e) => value=e.target.value}
+            value={props.endingTime}
+          />
+      </div>
     </div>
-    <div className='optionsInput'>
-        Wake Up
-        <input id='startingTime' 
-          data-tag="dayLength1"
-          className="lpInput"
-          type="text"
-          onChange={(e) => value=e.target.value}
-          value={props.startingTime}
-        />
+    <div className='row'>
+      <div className='col optionsInput'>
+        <strong>Daily Food Budget</strong>
+          <input id='foodCost' 
+            data-tag="allowanceForFood"
+            className="lpInput"
+            type="text"
+            onChange={(e) => value=e.target.value}
+            value={props.foodCostPerDay}
+          />
+      </div>
+      <div className='col optionsInput'>
+        <strong>Sleeping Preferences</strong>
+          <input id='nightlyCost' 
+            data-tag="sleep preference"
+            className="lpInput"
+            type="text"
+            onChange={(e) => value=e.target.value}
+            value={props.nightlyCost} 
+          />
+      </div>
     </div>
-    <div className='optionsInput'>
-        Come Back to Camp
-        <input id='endingTime' 
-          data-tag="dayLength2"
-          className="lpInput"
-          type="text"
-          onChange={(e) => value=e.target.value}
-          value={props.endingTime}
-        />
-    </div>
-    <div className='optionsInput'>
-      Daily Food Budget
-        <input id='foodCost' 
-          data-tag="allowanceForFood"
-          className="lpInput"
-          type="text"
-          onChange={(e) => value=e.target.value}
-          value={props.foodCostPerDay}
-        />
-    </div>
-    <div className='optionsInput'>
-      Sleeping Preferences
-        <input id='nightlyCost' 
-          data-tag="sleep preference"
-          className="lpInput"
-          type="text"
-          onChange={(e) => value=e.target.value}
-          value={props.nightlyCost} 
-        />
-    </div>
-    <div className='planButton'>
-      <button 
-        onClick= { (e) => {
-          props.setPreferences(+document.getElementById('foodCost').value, +document.getElementById('startingTime').value, +document.getElementById('endingTime').value, +document.getElementById('nightlyCost').value);
-          props.handleChange(setBudget, document.getElementById('budget').value);
-          props.handleChange(setLength, document.getElementById('time').value);
-          props.handleChange(setLocation, document.getElementById('location').value);
-          props.handleChange(setDistance, document.getElementById('distance').value);
+    <div className='row planButton'>
+      <div className='col'>
+        <Button bsStyle='primary' className='planButton2'
+          onClick= { (e) => {
+            props.setPreferences(+document.getElementById('foodCost').value, +document.getElementById('startingTime').value, +document.getElementById('endingTime').value, +document.getElementById('nightlyCost').value);
+            props.handleChange(setBudget, document.getElementById('budget').value);
+            props.handleChange(setLength, document.getElementById('time').value);
+            props.handleChange(setLocation, document.getElementById('location').value);
+            props.handleChange(setDistance, document.getElementById('distance').value);
+            }
           }
-        }
-      > Plan My Vacation 
-      </button>
+        > Plan My Vacation 
+        </Button>
+      </div>
     </div>
   </div>
   );
